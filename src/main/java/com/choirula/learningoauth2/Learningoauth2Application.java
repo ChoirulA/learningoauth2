b0e82c3c96f1af1f3b65239b4c0634143d0ae3da
Choirul Andriansyah <choirulandriansyah@10.1.1.30>
2021-06-07 10:54:54 +0700

    first learn oauth 2

---
+++ src/main/java/com/choirula/learningoauth2/Learningoauth2Application.java
@@ -0,0 +1,13 @@
+package com.choirula.learningoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learningoauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Learningoauth2Application.class, args);
+	}
+
+}
